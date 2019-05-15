f2de2468877e8dfae74e799a580141e30857f687
LPicinin <luishpj@outlook.com.br>
2019-05-15 09:33:45 -0300

    projeto dois

---
+++ projeto2/public/js/validacoes.js
@@ -0,0 +1,13 @@
+function valida()
+{
+    let email = document.getElementById("email").value;
+    if(email.trim().length > 0)
+    {
+
+    }
+}
+/*
+$('#D').text()//
+$('#D').html()//
+$('#D').append()//
+*/
